b32aee8eedace3bcc5d53ab19d479d6ba42423b0
suvarna <suvarnavadde77@gmail.com>
2026-02-04 12:31:22 -0600

    floatvs double

--- calculator.py
+++ calculator.py
@@ -1,7 +1,20 @@
-a = 25
-b = 2
+# a = 25
+# b = 2
 
-print("Addition:", a + b)
-print("Subtraction:", a - b)
-print("Multiplication:", a * b)
-print("Division:", a / b)
+# print("Addition:", a + b)
+# print("Subtraction:", a - b)
+# print("Multiplication:", a * b)
+# print("Division:", a / b)
+#################################################################
+
+public class FloatVsDouble {
+
+    public static void main(String[] args) {
+
+        float num1 = 10.5f;   // float needs 'f'
+        double num2 = 10.5;   // double does not need 'f'
+
+        System.out.println("Float value = " + num1);
+        System.out.println("Double value = " + num2);
+    }
+}
